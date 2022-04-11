64684775dae2c59cca45a9c97cae363e8b81b8b2
Eliyas Hossain <eliyashossain700@gmail.com>
2022-04-11 22:44:18 +0600

    Header Added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import logo from "../../../images/logo.png";
 
 const Header = () => {
   return (
     <div>
-      <h2>This is Header</h2>
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img className="w-50" src={logo} alt="" />
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 };
