f7e3cd5ce955e2db545b5eced7387fdd38a010df
Eliyas Hossain <eliyashossain700@gmail.com>
2022-04-13 23:20:49 +0600

    Sign In By Email and Password

--- src/Pages/LogIn/Login.js
+++ src/Pages/LogIn/Login.js
@@ -1,11 +1,19 @@
 import React, { useRef, useState } from "react";
 import { Button, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import auth from "../../firebase.init";
 
 const Login = () => {
   const [validated, setValidated] = useState(false);
   const emailRef = useRef("");
   const passwordRef = useRef("");
+  const navigate = useNavigate();
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+  if (user) {
+    navigate("/about");
+  }
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
@@ -18,7 +26,7 @@ const Login = () => {
     setValidated(true);
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
-    console.log(email, password);
+    signInWithEmailAndPassword(email, password);
   };
   return (
     <div className=" container w-50 mt-5 pt-4 pb-5">
@@ -59,6 +67,7 @@ const Login = () => {
             Invaild Email.
           </Form.Control.Feedback>
         </Form.Group>
+        <p className="text-danger">{error}</p>
         <Button variant="primary" type="submit">
           Log In
         </Button>
