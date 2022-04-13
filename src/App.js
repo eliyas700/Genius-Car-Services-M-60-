9234879d19420e93cdadf68d6b82e1c8d65cf05f
Eliyas Hossain <eliyashossain700@gmail.com>
2022-04-13 10:21:01 +0600

    Set Up deynamic Route Using Params and Navigate

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import About from "./Pages/About/About";
 import Footer from "./Pages/Shared/Footer/Footer";
 import Header from "./Pages/Shared/Header/Header";
 import HomePage from "./Pages/Home/HomePage/HomePage";
+import ServiceDetail from "./Pages/ServiceDetails/ServiceDetail";
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
+        <Route
+          path="/service/:serviceId"
+          element={<ServiceDetail></ServiceDetail>}
+        ></Route>
         <Route path="/about" element={<About />}></Route>
       </Routes>
       <Footer></Footer>
--- src/Pages/Home/Service.js
+++ src/Pages/Home/Service.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Services.css";
 
 const Service = ({ service }) => {
-  const { name, img, description, price } = service;
+  const { id, name, img, description, price } = service;
+  const navigate = useNavigate();
+  const handleServiceDetail = (id) => {
+    navigate(`service/${id}`);
+  };
   return (
     <div className=" service-name">
       <img className="" src={img} alt="" />
@@ -12,7 +17,12 @@ const Service = ({ service }) => {
           <small>{description}</small>
         </p>
         <h4> $ {price}</h4>
-        <button className="btn btn-primary">Book {name}</button>
+        <button
+          onClick={() => handleServiceDetail(id)}
+          className="btn btn-primary"
+        >
+          Book {name}
+        </button>
       </div>
     </div>
   );
