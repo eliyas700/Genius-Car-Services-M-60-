129060ffbd7e139b0464f4e3106b76e12dca7388
Eliyas Hossain <eliyashossain700@gmail.com>
2022-04-18 17:27:10 +0600

    Check Deploy Firebase

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -5,29 +5,12 @@ const About = () => {
     <div className="mt-5 pt-2">
       <h3>ABout Section</h3>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-        aliquid perferendis possimus non enim iure quaerat reprehenderit omnis
-        qui atque porro voluptatem sequi provident totam nobis eum officia
-        accusantium, obcaecati repellat expedita nihil tempore dolorum unde.
-        Beatae, dolorum modi! Voluptatibus, nemo nulla ullam molestiae nihil
-        autem sint iusto numquam. Consequatur impedit iusto aspernatur
-        doloremque tempore! Tempora natus exercitationem blanditiis fugiat.
-        Reprehenderit, veritatis sapiente reiciendis itaque veniam, aut
-        similique neque aperiam voluptatibus harum soluta praesentium earum
-        numquam laudantium obcaecati? Odio eaque deserunt voluptate dolorem
-        temporibus minima numquam optio eius explicabo corrupti, dicta veritatis
-        harum facilis nam ea, reprehenderit itaque quidem illo nulla neque
-        animi. Ipsum nesciunt tempora beatae quo eligendi sequi expedita
-        temporibus ut doloribus quia? Consequatur accusantium voluptas
-        recusandae atque eligendi a saepe voluptatibus voluptatem, rerum quas
-        amet hic commodi, explicabo repudiandae corporis error ullam maiores
-        numquam molestiae odio asperiores. Quisquam corporis tenetur ut nisi
-        repellat maxime distinctio quia necessitatibus temporibus et porro odit
-        itaque, ipsa reiciendis facilis veritatis esse cum, autem mollitia
-        corrupti. Velit recusandae deserunt optio nisi eaque. Repellendus a nemo
-        quisquam quaerat odio quis amet sequi rerum distinctio error quasi
-        beatae dignissimos dolorum ratione iste, ducimus aliquam at placeat nisi
-        deleniti dolores natus. Vel nobis perferendis earum.
+        Hi this is me eliyas .ipsa reiciendis facilis veritatis esse cum, autem
+        mollitia corrupti. Velit recusandae deserunt optio nisi eaque.
+        Repellendus a nemo quisquam quaerat odio quis amet sequi rerum
+        distinctio error quasi beatae dignissimos dolorum ratione iste, ducimus
+        aliquam at placeat nisi deleniti dolores natus. Vel nobis perferendis
+        earum.
       </p>
     </div>
   );
